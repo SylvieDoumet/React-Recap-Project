cdbff618a15c40ef41c9c108d0081a550cc65f18
Sylvie Doumet <sylviedoumet@web.de>
2024-06-18 12:31:49 +0200

    uselocalstorage-problem-resolved_for_colorInput

--- src/Components/ColorInput/ColorInput.jsx
+++ src/Components/ColorInput/ColorInput.jsx
@@ -1,9 +1,16 @@
-import { useLocalStorageState } from "use-local-storage-state";
+import { useState, useEffect } from "react";
 
 // Issue 5 : Local storage state package
 
 function ColorInput({ id, defaultValue }) {
-  const [inputValue, setInputValue] = useLocalStorageState(id, defaultValue);
+  const [inputValue, setInputValue] = useState(() => {
+    const savedValue = localStorage.getItem(id);
+    return savedValue ? JSON.parse(savedValue) : defaultValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(id, JSON.stringify(inputValue));
+  }, [id, inputValue]);
 
   function handleInputValue(event) {
     setInputValue(event.target.value);
