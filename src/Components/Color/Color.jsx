9f87ad97b8af4ee40ce017f6e34367bca058b80f
Sylvie Doumet <sylviedoumet@web.de>
2024-06-18 10:07:48 +0200

    resolved_problems_within_code

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -8,40 +8,39 @@ import ColorForm from "../ColorForm/ColorForm";
 // Issue 3 : Delete Color - Add Confirmation Deletion - State (initially set to false)
 // Issue 4: Edit Color - Add Edit - State (initially set to false)
 
-function Color({ color, onDelete, onUpdate }) {
+export function Color({ id, hex, role, contrastText, onDelete, onUpdate }) {
   const [isConfirmingDelete, setIsConfirmingDelete] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [contrastScore, setContrastScore] = useState(null);
 
 async function checkContrast () {
-  const response = await fetch("https://www.aremycolorsaccessible.com/api/are-they"),
-      {
+  const response = await fetch("https://www.aremycolorsaccessible.com/api/are-they", {
         method: "POST",
-        body: JSON.stringify({ color: color.hex, contrast: color.contrastText }),
+        body: JSON.stringify({ color: hex, contrast: contrastText }),
         headers: {
           "Content-Type": "application/json",
         },
-      }
+      });
     const data = await response.json();
     setContrastScore(data.score);
   };
 
   useEffect(() => {
     checkContrast();
-  }, [color.hex, color.contrastText]);
+  }, [hex, contrastText]);
 
 
   return (
     <div
       className="color-card"
       style={{
-        background: color.hex,
-        color: color.contrastText,
+        background: hex,
+        color: contrastText,
       }}
     >
-      <h3 className="color-card-headline">{color.hex}</h3>
-      <h4>{color.role}</h4>
-      <p>contrast: {color.contrastText}</p>
+      <h3 className="color-card-headline">{hex}</h3>
+      <h4>{role}</h4>
+      <p>contrast: {contrastText}</p>
       {/* Issue 3: Delete Color - Step 1 Add a DELETE button to each color card and passed it a isConfirmingDelete function which opens up a confirmation window with a 
       message (Are you sure you want to delete?) and two buttons (Yes, No) */}
       {/* Button - DELETE : Opens up the cofirmation windows to choose between the YES, I want to delete. and NO, I don't want to delete - button   */}
@@ -57,7 +56,7 @@ async function checkContrast () {
           </p>
 
           {/* Button - YES : Deletes the color via its ID  */}
-          <button onClick={() => onDelete(color.id)}>Yes</button>
+          <button onClick={() => onDelete(id)}>Yes</button>
 
           {/* Button - NO : sets the useState back to initial (ConfirmingDelete ? false)  */}
           <button onClick={() => setIsConfirmingDelete(false)}>No</button>
@@ -68,9 +67,9 @@ async function checkContrast () {
       {isEditing && (
         <div className="editing-dialog">
           <ColorForm
-            initialData={color}
+            initialData={Color}
             onSubmitColor={(updatetColor) => {
-              onUpdate({ ...color, ...updatetColor });
+              onUpdate({ ...Color, ...updatetColor });
               setIsEditing(false);
             }}
             onCancel={() => setIsEditing(false)}
@@ -81,4 +80,3 @@ async function checkContrast () {
     </div>
   );
 
-export default Color;
