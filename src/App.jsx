f6a5e75ab19b288d60581b641dfbc94fc0a6a1a6
Sylvie Doumet <sylviedoumet@web.de>
2024-06-18 11:53:06 +0200

    Issue8-resolved

--- src/App.jsx
+++ src/App.jsx
@@ -4,73 +4,175 @@ import "./App.css";
 
 // Basics - create unique ID, create Color Form
 
-// import { useState } from "react";
+import { useState } from "react";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import { nanoid } from "nanoid";
 import { useLocalStorageState } from "use-local-storage-state";
 
-function App() {
+// Issue #8 Theme - import initialThemes
+import { initialThemes } from "./Components/Themes/InitialThemes";
+
+export function App() {
   // Issue #2 Add Color  - introduce state to manage color adding
   // const [colors, setColors] = useState(initialColors);
 
-  const [colors, setColors] = useLocalStorageState("colors", []);
-  const [editColorId, setEditColorId] = useLocalStorageState(
-    "editColorId",
-    null
-  );
+  // const [colors, setColors] = useLocalStorageState("colors", []);
+  // const [editColorId, setEditColorId] = useLocalStorageState(
+  //   "editColorId",
+  //   null
+  // );
 
-  // Issue #2 Add Color - add function to handle color adding
+  // Issue #8 Theme - introduce state to manage themes
+
+  const [themes, setThemes] = useLocalStorageState("themes", initialThemes);
+  const [currentThemeId, setCurrentThemeId] = useState(themes[0].id);
+
+  // // Issue #2 Add Color - add function to handle color adding
+  // function handleAddColor(newColor) {
+  //   setColors([{ ...newColor, id: nanoid() }, ...colors]);
+  // }
+
+  // // Issue #3 Delete Color - add function to handle color deletion
+
+  // function handleDeleteColor(id) {
+  //   setColors(colors.filter((color) => color.id !== id));
+  // }
+
+  // // Issue #4 Edit Color - add function to handle color update
+
+  // function handleUpdateColor(updatedColor) {
+  //   console.log("update color", updatedColor);
+  //   setColors(
+  //     colors.map((color) =>
+  //       color.id === updatedColor.id ? updatedColor : color
+  //     )
+  //   );
+  // }
+  // function handleEditColor(colorId) {
+  //   setEditColorId(colorId);
+  // }
+
+  // // Issue #4 Edit Color - add function to handle color update
+
+  // function handleUpdateColor(updatedColor) {
+  //   console.log("update color", updatedColor);
+  //   setColors(
+  //     colors.map((color) =>
+  //       color.id === updatedColor.id ? updatedColor : color
+  //     )
+  //   );
+  // }
+  // function handleEditColor(colorId) {
+  //   setEditColorId(colorId);
+  // }
+
+  // Issue #8 Theme - add function to handle theme selection for deletion and update
+
   function handleAddColor(newColor) {
-    setColors([{ ...newColor, id: nanoid() }, ...colors]);
-  }
-
-  // Issue #3 Delete Color - add function to handle color deletion
-
-  function handleDeleteColor(id) {
-    setColors(colors.filter((color) => color.id !== id));
-  }
-
-  // Issue #4 Edit Color - add function to handle color update
-
-  function handleUpdateColor(updatedColor) {
-    console.log("update color", updatedColor);
-    setColors(
-      colors.map((color) =>
-        color.id === updatedColor.id ? updatedColor : color
-      )
+    setThemes(
+      themes.map((theme) => {
+        if (theme.id === currentThemeId) {
+          return {
+            ...theme,
+            colors: [{ ...newColor, id: nanoid() }, ...theme.colors],
+          };
+        }
+        return theme;
+      })
     );
   }
-  function handleEditColor(colorId) {
-    setEditColorId(colorId);
+
+  function handleDeleteColor(id) {
+    setThemes(
+      themes.map((theme) => {
+        if (theme.id === currentThemeId) {
+          return {
+            ...theme,
+            colors: theme.colors.filter((color) => color.id !== id),
+          };
+        }
+        return;
+      })
+    );
   }
+
+  function handleUpdateColor(updatedColor) {
+    setThemes(
+      themes.map((theme) => {
+        if (theme.id === currentThemeId) {
+          return {
+            ...theme,
+            colors: theme.colors.map((color) =>
+              color.id === updatedColor.id ? updatedColor : color
+            ),
+          };
+        }
+        return theme;
+      })
+    );
+  }
+
+  const currentTheme = themes.find((theme) => theme.id === currentThemeId);
+
+  // Return statement for all Issues till Issue 7
+
+  //   return (
+  //     <div className="app">
+  //       <h1>Theme Creator</h1>
+
+  //       {/* Issue #3 Delete Color  - add function to display message when no colors are left */}
+  //       {colors.length === 0 ? (
+  //         <p>No colors present. Please add new colors.</p>
+  //       ) : (
+  //         colors.map((color) => (
+  //           <Color
+  //             key={color.id}
+  //             color={color}
+  //             onDelete={handleDeleteColor}
+  //             onUpdate={handleUpdateColor}
+  //             onEdit={handleEditColor}
+  //             isEditing={editColorId === color.id}
+  //           />
+  //         ))
+  //       )}
+  //       {colors.map((color, index) => (
+  //         <ColorForm
+  //           key={index}
+  //           initialData={color}
+  //           onAddColor={handleAddColor}
+  //         />
+  //       ))}
+  //     </div>
+  //   );
+  // }
+  // export default App;
+
+  // Updatet Return Statement for Issue 8 based on Current Theme
 
   return (
     <div className="app">
       <h1>Theme Creator</h1>
-
-      {/* Issue #3 Delete Color  - add function to display message when no colors are left */}
-      {colors.length === 0 ? (
-        <p>No colors present. Please add new colors.</p>
-      ) : (
-        colors.map((color) => (
-          <Color
-            key={color.id}
-            color={color}
-            onDelete={handleDeleteColor}
-            onUpdate={handleUpdateColor}
-            onEdit={handleEditColor}
-            isEditing={editColorId === color.id}
-          />
-        ))
-      )}
-      {colors.map((color, index) => (
-        <ColorForm
-          key={index}
-          initialData={color}
-          onAddColor={handleAddColor}
+      <select
+        value={currentThemeId}
+        onChange={(e) => setCurrentThemeId(e.target.value)}
+      >
+        {themes.map((theme) => (
+          <option key={theme.id} value={theme.id}>
+            {theme.name}
+          </option>
+        ))}
+      </select>
+      {currentTheme.colots.map((color, index) => (
+        <Color
+          key={color.id}
+          color={color}
+          onDelete={handleDeleteColor}
+          onUpdate={handleUpdateColor}
+          isEditing={false}
         />
       ))}
+
+      <ColorForm onAddColor={handleAddColor} />
     </div>
   );
 }
-export default App;
